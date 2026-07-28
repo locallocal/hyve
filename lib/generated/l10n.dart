3b142971f647ce78b32872b83716d8efc67d8aed
locallocal <locallocal@163.com>
2026-07-28 23:48:44 +0800

    chore: format generated localization file

--- lib/generated/l10n.dart
+++ lib/generated/l10n.dart
@@ -1,6 +1,7 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+
 import 'intl/messages_all.dart';
 
 // **************************************************************************
@@ -1454,8 +1455,7 @@ class S {
     return Intl.message(
       'Generating…',
       name: 'generating',
-      desc:
-          'Disabled primary action label while a request is running and cannot be cancelled',
+      desc: 'Disabled primary action label while a request is running and cannot be cancelled',
       args: [],
     );
   }
